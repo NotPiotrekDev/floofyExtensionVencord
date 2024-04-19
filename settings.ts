7cadcbc6ffb727e957c1c1eeaa2892d3861ff6fb
NotPiotrekDev <131684386+NotPiotrekDev@users.noreply.github.com>
2024-04-19 13:38:36 +0200

    more cool options

--- settings.ts
+++ settings.ts
@@ -6,5 +6,52 @@ export const settings = definePluginSettings({
         type: OptionType.BOOLEAN,
         description: "Change message text",
         default: true,
+        restartNeeded: true
+    },
+    customBadgeCode: {
+        type: OptionType.STRING,
+        description: "Enter secret code to get access to custom badges",
+    },
+    devBadge: {
+        type: OptionType.BOOLEAN,
+        description: "Get Floofy Developer badge [Only for dev]",
+        default: false,
+        restartNeeded: true
+    },
+    BoykisserBadge: {
+        type: OptionType.BOOLEAN,
+        description: "Get Certified Boykisser badge [Code required]",
+        default: false,
+        restartNeeded: true
+    },
+    FurryBadge: {
+        type: OptionType.BOOLEAN,
+        description: "Get Certified Furry badge [Code required]",
+        default: false,
+        restartNeeded: true
+    },
+    ProtogenBadge: {
+        type: OptionType.BOOLEAN,
+        description: "Get Certified Protogen badge [Code required]",
+        default: false,
+        restartNeeded: true
+    },
+    ColonThreeBadge: {
+        type: OptionType.BOOLEAN,
+        description: "Get :3 badge [Code required]",
+        default: false,
+        restartNeeded: true
+    },
+    femboyBadge: {
+        type: OptionType.BOOLEAN,
+        description: "Get Certified Femboy badge [Code required]",
+        default: false,
+        restartNeeded: true
+    },
+    NoneBadge: {
+        type: OptionType.BOOLEAN,
+        description: "Remove all badges",
+        default: true,
+        restartNeeded: true
     },
 });
